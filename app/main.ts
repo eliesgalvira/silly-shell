000fbee4cb054bca29a3060434a27f96df6b0e5e
Elies <68759092+eliesgalvira@users.noreply.github.com>
2025-11-26 00:17:32 +0100

    Refactored code and wrote the type builtin

--- app/main.ts
+++ app/main.ts
@@ -6,10 +6,24 @@ const rl = createInterface({
 
 const builtins = {
   ECHO: "echo",
+  TYPE: "type",
   EXIT: "exit"
  } as const;
 
 type Builtins = (typeof builtins)[keyof typeof builtins];
+
+const builtinValues: Set<string> = new Set(Object.values(builtins));
+
+// Type guard
+const isBuiltin = (command: string): command is Builtins => {
+  return builtinValues.has(command);
+}
+
+const commandHandlers:  Record<Builtins, (args: string[]) => void> = {
+  [builtins.ECHO]: (args) => console.log(args.join(" ")),
+  [builtins.TYPE]: (args) => typeBuiltin(args),
+  [builtins.EXIT]: () => process.exit(0),
+};
 
 rl.setPrompt("$ ");
 rl.prompt();
@@ -20,21 +34,31 @@ rl.on("line", (line) => {
   const commandName = commandFields[0];
   const commandArgs = commandFields.slice(1);
 
-  if (command === "exit 0" || command === builtins.EXIT) {
-    process.exit(0);
+  if (!command) {
+    rl.prompt();
+    return;
   }
 
-  else if (commandName === builtins.ECHO) {
-    console.log(commandArgs.join(" "));
-    rl.prompt();
-  }
-  
-  else if (commandName) {
+  if (isBuiltin(commandName)) {
+    commandHandlers[commandName](commandArgs);
+  } else {
     console.log(`${commandName}: not found`);
   }
+
   rl.prompt();
 });
 
 rl.on("close", () => {
   process.exit(0);
 });
+
+
+const typeBuiltin = (args: string[]) => {
+  args.forEach((possibleBuiltin) => {
+    if (builtinValues.has(possibleBuiltin)) {
+      console.log(`${possibleBuiltin} is a shell builtin`);
+    } else {
+      console.log(`${possibleBuiltin}: not found`)
+    }
+  })
+}
